614d9dfa179c8d8e6d04f60dcd12cd33cf485c7f
raja138 <62994530+raja138@users.noreply.github.com>
2023-09-20 22:42:37 +0530

    Update stackBalancedParenthesis.cpp

--- stackBalancedParenthesis.cpp
+++ stackBalancedParenthesis.cpp
@@ -1,1 +1,73 @@
 
+#include<iostream>
+#include<stack>
+using namespace std;
+
+bool isbalanced(char *arr)
+{
+    stack<char>s;
+	for(int i=0;arr[i]!='\0';i++)
+	{
+		char ch=arr[i];
+       switch(ch)
+	   {
+		   case '(':
+		   case '[':
+		   case '{':s.push(ch);
+		   break;
+		   case '}':
+		      if(!s.empty() && s.top()=='{')
+			  {
+				  s.pop();
+			  }
+			  else
+			  {
+				  return false;
+			  }     
+			  break;
+		  case ')':
+		      if(!s.empty() && s.top()=='(')
+			  {
+				  s.pop();
+			  }
+			  else
+			  {
+				  return false;
+			  }     
+			  break;
+          case ']':
+		      if(!s.empty() && s.top()=='[')
+			  {
+				  s.pop();
+			  }
+			  else
+			  {
+				  return false;
+			  }     
+			  break;
+
+	   }
+
+	}
+  if(s.empty())
+  {
+	  return true;
+  }
+return false;
+}
+
+int main() {
+ char arr[100]; 
+ 
+  cin.getline(arr, sizeof(arr));
+
+ if (isbalanced(arr))
+  {
+    cout << "Yes";
+  }
+  else
+  {
+    cout << "No";
+  }
+	return 0;
+}
